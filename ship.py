d2b85df23c3e7241048c3e3e2554ae320a099a89
FilipchukB <bohdanfil2015@gmail.com>
2019-11-06 10:05:43 +0200

    add file

---
+++ ship.py
@@ -0,0 +1,15 @@
+import pygame
+class Ship():
+    def  __init__(self, screen):
+
+         self.screen = screen
+ # Load the ship image and get its rect.
+         self.image = pygame.image.load('ship.png')
+         self.rect = self.image.get_rect()
+         self.screen_rect = screen.get_rect()
+ # Start each new ship at the bottom center of the screen.
+         self.rect.centerx = self.screen_rect.centerx
+         self.rect.bottom = self.screen_rect.bottom
+    def blitme(self):
+
+        self.screen.blit(self.image, self.rect)
